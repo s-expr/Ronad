bffb283b972ed55ca0e134e8597cc17b2ab281bc
s-expr <darren.fitzgerld@gmail.com>
2025-08-19 00:43:50 -0400

    Tile number enumeration and order

--- app/Tile.hs
+++ app/Tile.hs
@@ -1,7 +1,7 @@
 module Tile where
 
 data Tile 
-  = Standard Suit Number
+  = Number Suit Number
   | Wind Cardinal
   | Dragon DColor
   deriving (Eq, Show)
@@ -18,7 +18,7 @@ data Number
   | Eight
   | Nine 
   | Ten
-  deriving (Eq, Show)
+  deriving (Eq, Ord, Show)
 
 data Suit 
   = Manzu
@@ -38,10 +38,41 @@ data DColor
   | Red
   | Green
   deriving (Eq, Show)
-  
+ 
+instance Enum Number where
+  fromEnum One = 1
+  fromEnum Two = 2
+  fromEnum Three = 3
+  fromEnum Four = 4
+  fromEnum Five = 5
+  fromEnum RFive = 5
+  fromEnum Six = 6
+  fromEnum Seven = 7
+  fromEnum Eight = 8
+  fromEnum Nine = 9
+
+  toEnum num 
+    | num > 9 || num < 1= error "Invalid conversion"
+  toEnum 1 = One 
+  toEnum 2 = Two  
+  toEnum 3 = Three  
+  toEnum 4 = Four  
+  toEnum 5 = Five  
+  toEnum 6 = Six  
+  toEnum 7 = Seven  
+  toEnum 8 = Eight  
+  toEnum 9 = Nine  
+ 
+instance Ord Number where  
+  compare n1 n2 = compare (fromEnum n1) (fromEnum n2)
 
 
- terminal ::  Tile -> Bool
- terminal Standard _ One = True
- terminal Standard _ Nine = True
- terminal _ = False
+
+
+isTerminal :: Tile -> Bool 
+isTerminal Number _ One = True
+isTerminal Number _ Nine = True
+isTerminal _ = False
+
+normFive :: Number -> Number
+normFive RFive = Five
